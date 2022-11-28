3ffa73855f1225ca3674d91ca5ecf057d97c82a5
민석 <80371412+MinSeok0123@users.noreply.github.com>
2022-11-28 09:20:21 +0900

    guidevideo

--- src/components/Guide.js
+++ src/components/Guide.js
@@ -1,9 +1,29 @@
 import "./Guide.css";
+import ReactPlayer from "react-player";
+import styled from "styled-components";
 
-export function Guide () {
-    return (
-        <div>
-       <img className="Guiderecognize" src="/img/recognize.gif"></img>
-        </div>
-    );
+export function Guide() {
+  const PlayerWrapper = styled.div`
+    position: relative;
+    padding-top: 56.25% /* Player ratio: 100 / (1280 / 720) */;
+    .react-player {
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+    }
+  `;
+  return (
+    <PlayerWrapper>
+      <ReactPlayer
+        className="react-player"
+        url="https://www.youtube.com/watch?v=qSnsHO5v2GY&ab_channel=%EC%9D%B4%EB%AF%BC%EC%84%9D"
+        width="100%"
+        height="100vh"
+        muted={false} //chrome정책으로 인해 자동 재생을 위해 mute 옵션을 true로 해주었다.
+        playing={true}
+        loop={true}
+      />
+    </PlayerWrapper>
+  );
 }
